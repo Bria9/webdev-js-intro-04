4a4c640d1002b3c3e7e404a635b8bb8984e17f31
Bri A <alexiellis22@gmail.com>
2024-12-29 13:01:57 -0500

    Complete index.js experiment section and comment out code/Update README.md

--- js/index.js
+++ js/index.js
@@ -1,3 +1,65 @@
 // Prevent us from attempting to use variables
 // that are not declared
-"use strict"
+"use strict";
+
+
+/* 
+let healthPoints = 20; //Output: Hero is healthy.
+
+if (healthPoints >= 10) {
+  console.log("Hero is healthy.");
+} else if (healthPoints < 10 && healthPoints >= 5) {
+  console.log("Hero is slightly damaged.");
+} else {
+  console.log("Hero is close to death.");
+}
+
+// if statement
+
+let proMember = true;
+let paidStatus = false;
+
+if (paidStatus) {
+  console.log(`User's Pro Membership status is ${proMember}`);
+} else {
+  proMember = false;
+  console.log(`User's Pro Membership status is ${proMember}`);
+}
+let student = true;
+let nurse = false;
+
+if (student || nurse) {
+  console.log("discount");
+} else {
+  console.log("no discount");
+}
+
+//else if
+
+let rewardPoints = 500;
+
+if (rewardPoints >= 500) {
+  console.log("you've earned enough for 1 small drink 🧋");
+} else if (rewardPoints >= 500 && rewardPoints <= 1000) {
+  console.log("You've earned enough for 1 bakery item 🥯");
+} else if (rewardPoints >= 1000) {
+  console.log("You've earned enough for 1 free meal 🍔");
+} else {
+  console.log("You have no rewards");
+}
+
+let youtubeGoldAward = false;
+let gold = 1000000;
+let silver = 100000;
+let subscribers = 40000;
+
+if (subscribers >= gold) {
+  youtubeGoldAward = true;
+  console.log("congratulations Gold Member🏅");
+} else if (subscribers >= silver && subscribers < gold) {
+  console.log("congratulations Silver member🥈");
+} else {
+  let difference = silver - subscribers;
+  console.log(`you're ${difference} points away from becoming a Silver member`);
+}
+ */
